7fefd1e4bc66270b6fd379a789dce894a327c640
ImBIOS <imamuzzaki@gmail.com>
2024-01-27 07:10:01 +0000

    Write a function to validate text length

--- public/script.js
+++ public/script.js
@@ -4,3 +4,22 @@ const summarizedTextArea = document.getElementById("summary");
 
 textArea.addEventListener("input", verifyTextLength);
 submitButton.addEventListener("click", submitData);
+
+// First, we disable the submit button by default when the user loads the website.
+submitButton.disabled = true;
+
+// Next, we define a function called verifyTextLength(). This function will be called when the user enters something in the text area. It receives an event, called ‘e’ here
+function verifyTextLength(e) {
+
+  // The e.target property gives us the HTML element that triggered the event, which in this case is the textarea. We save this to a variable called ‘textarea’
+  const textarea = e.target;
+
+  // Check if the text in the text area is the right length - between 200 and 100,000 characters
+  if (textarea.value.length > 200 && textarea.value.length < 100000) {
+    // If it is, we enable the submit button.
+    submitButton.disabled = false;
+  } else {
+    // If it is not, we disable the submit button.
+    submitButton.disabled = true;
+  }
+}
