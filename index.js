849e87b880714e748cede799bb878c1413cdf60a
ImBIOS <imamuzzaki@gmail.com>
2024-01-27 07:06:56 +0000

    Finish the POST /summarize endpoint

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const summarizeText = require('./summarize.js');
 
 // Parses JSON bodies (as sent by API clients)
 app.use(express.json());
@@ -10,10 +11,17 @@ app.use(express.static('public'));
 
 // Handle POST requests to the '/summarize' endpoint
 app.post('/summarize', (req, res) => {
+  // get the text_to_summarize property from the request body
+  const text = req.body.text_to_summarize;
 
-   // TODO: handle POST /summarize request
-
-
+  // call your summarizeText function, passing in the text from the request
+  summarizeText(text)
+    .then(response => {
+      res.send(response); // Send the summary text as a response
+    })
+    .catch(error => {
+      console.log(error.message);
+    });
 });
 
 // Start the server
