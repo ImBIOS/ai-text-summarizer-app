235515d4a559a3fa479a4f7e0604007c3dfa2bf2
ImBIOS <imamuzzaki@gmail.com>
2024-01-27 06:51:47 +0000

    Make a summarizeText() function

---
+++ summarize.js
@@ -0,0 +1,11 @@
+// This is the function where the call to the API is made. Returns the summarized text as a string.
+
+async function summarizeText(text) {
+
+  // INSERT CODE SNIPPET FROM POSTMAN BELOW
+
+}
+
+// Allows for summarizeText() to be called outside of this file
+
+module.exports = summarizeText;
