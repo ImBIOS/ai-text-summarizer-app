daf7216a2f9c71e9de22a75a6dd1e527d9bfa06d
ImBIOS <imamuzzaki@gmail.com>
2024-01-27 06:51:09 +0000

    Add a /summarize endpoint in index.js

--- index.js
+++ index.js
@@ -8,6 +8,14 @@ app.use(express.json());
 // Serves static files from the 'public' directory
 app.use(express.static('public'));
 
+// Handle POST requests to the '/summarize' endpoint
+app.post('/summarize', (req, res) => {
+
+   // TODO: handle POST /summarize request
+
+
+});
+
 // Start the server
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}/`);
