76a29d1fe2c2fa392d8c32500f596a9c274a4191
ImBIOS <imamuzzaki@gmail.com>
2024-01-27 07:09:02 +0000

    Adding event listeners

--- public/script.js
+++ public/script.js
@@ -1,5 +1,6 @@
 const textArea = document.getElementById("text_to_summarize");
-
 const submitButton = document.getElementById("submit-button");
-
 const summarizedTextArea = document.getElementById("summary");
+
+textArea.addEventListener("input", verifyTextLength);
+submitButton.addEventListener("click", submitData);
