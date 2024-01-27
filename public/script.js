5c42b6f13a0214540d100a8ba66f8391742abbaf
ImBIOS <imamuzzaki@gmail.com>
2024-01-27 07:08:12 +0000

    Creating references to each interactive HTML element

---
+++ public/script.js
@@ -0,0 +1,5 @@
+const textArea = document.getElementById("text_to_summarize");
+
+const submitButton = document.getElementById("submit-button");
+
+const summarizedTextArea = document.getElementById("summary");
